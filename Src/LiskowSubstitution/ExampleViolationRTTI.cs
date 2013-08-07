e4540dcba537e20543910a1a85af2d08e499f88a
alessioperry <ale.pam@gmail.com>
2013-08-07 22:13:03 +0200

    Added a liskov violationg principles examples added reference to nunit

--- Src/LiskowSubstitution/ExampleViolationRTTI.cs
+++ Src/LiskowSubstitution/ExampleViolationRTTI.cs
@@ -2,6 +2,9 @@
 {
     class ExampleViolationRTTI
     {
+
+        //this is a violation of Liskov Substitution
+        //calculateAreas have to know all the derived classes of a shaoe to work well
         public double calculateAreas(Shape shape)
         {
             var circle = (Circle) shape;
@@ -10,6 +13,76 @@
             //test
 
             return circle == null ? rectangle.Area() : circle.Area();
+        }
+    }
+
+
+    public class ViolationOfLiskov
+    {
+        public void violation()
+        {
+            var square = new SquareViolationg();
+            square.SetHeight(30);
+
+            TheViolation(square);
+
+            //the heicht and the width are not the same.
+        }
+
+        private void TheViolation(rettangleViolating rectangle)
+        {
+            rectangle.SetHeight(34);
+        }
+    }
+
+    public class rettangleViolating
+    {
+        private double width;
+        private double height;
+
+        public void SetHeight(double height)
+        {
+            this.height = height;
+        }
+
+        public double GetHeight()
+        {
+            return height;
+        }
+
+        public void SetWidth(double width)
+        {
+            this.width = width;
+        }
+
+        public double GetWidth()
+        {
+            return width;
+        }
+    }
+
+    public class SquareViolationg : rettangleViolating
+    {
+        public new void SetHeight(double height)
+        {
+            base.SetHeight(height);
+            base.SetWidth(height);
+        }
+
+        public new double GetHeight()
+        {
+            return base.GetHeight();
+        }
+
+        public new void SetWidth(double width)
+        {
+            base.SetHeight(width);
+            base.SetWidth(width);
+        }
+
+        public new double GetWidth()
+        {
+            return base.GetWidth();
         }
     }
 }
