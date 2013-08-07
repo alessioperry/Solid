58954213c0a74fe4723cd49bd51a27ff0ab87d6a
alessioperry <ale.pam@gmail.com>
2013-08-07 12:07:27 +0200

    added comment

---
+++ Src/LiskowSubstitution/ExampleViolationRTTI.cs
@@ -0,0 +1,15 @@
+﻿namespace LiskowSubstitution
+{
+    class ExampleViolationRTTI
+    {
+        public double calculateAreas(Shape shape)
+        {
+            var circle = (Circle) shape;
+            var rectangle = (Rectangle) shape;
+
+            //test
+
+            return circle == null ? rectangle.Area() : circle.Area();
+        }
+    }
+}
